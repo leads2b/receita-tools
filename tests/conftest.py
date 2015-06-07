41df3029c83d3604204960257da20e8ca90b2cc6
Vinicius Kwiecien Ruoso <vinicius.ruoso@gmail.com>
2015-06-07 16:37:23 -0300

    Added a initial version of tests

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+
+import os
+import sys
+
+# Add module to the path
+sys.path.insert(0, (os.path.join(os.path.dirname(__file__), '..')))
---
+++ tests/test_client.py
@@ -0,0 +1,9 @@
+
+
+from receita.tools import client
+
+
+class TestClient(object):
+
+    def test_client_returns_json_object(self):
+        pass
